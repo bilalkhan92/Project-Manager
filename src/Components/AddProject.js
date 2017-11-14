1a7accc5e7a2af4d9efd5e0a1d335cf2bafa2f5d
Bilal <bkhan@inboxmarketer.com>
2017-11-14 11:25:32 -0500

    first commit

---
+++ src/Components/AddProject.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class AddProject extends Component {
+  static defaultProps = {
+    categories: ['Web Design', 'Web Development', 'Mobile Development']
+  }
+  render() {
+    let categoryOptions = this.props.categories.map(function(category){
+      return(
+        <option key={category} value="category">{category}</option>
+      );
+    });
+    return (
+      <div>
+        <h3>Add Project</h3>
+        <form>
+          <div>
+
+            <label>Title</label><br />
+            <input type="text" ref="title" />
+            <br/>
+            <label>Category</label><br />
+            <select ref="category">
+              {categoryOptions}
+            </select>
+
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default AddProject;
